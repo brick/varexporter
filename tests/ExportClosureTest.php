5421bd004575870bd8e873bd7be12f8afff9a3d2
Benjamin Morel <benjamin.morel@gmail.com>
2019-04-08 15:34:28 +0200

    Closure tests

---
+++ tests/ExportClosureTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\VarExporter\Tests;
+
+use Brick\VarExporter\Tests\Classes\PublicPropertiesOnly;
+
+/**
+ * Tests exporting closures.
+ */
+class ExportClosureTest extends AbstractTestCase
+{
+    public function testExportSimpleClosure()
+    {
+        $var = function() {
+            echo 'Hello, world!';
+        };
+
+        $expected = <<<'PHP'
+function () {
+    echo 'Hello, world!';
+}
+PHP;
+
+        $this->assertExportEquals($expected, $var);
+    }
+
+    public function testExportNestedComplexClosure()
+    {
+        $var = [
+            (object) [
+                'callback' => function() {
+                    return function (PublicPropertiesOnly $a, int $b, string & $c, string ...$d) : ?string {
+                        $a->foo += $b;
+                        $c = $a->bar;
+                        $a->bar = implode('', $d);
+
+                        $this->someProp = [
+                            $a->foo,
+                            $a->bar,
+                            $c
+                        ];
+
+                        return $this->someProp['c'];
+                    };
+                }
+            ]
+        ];
+
+        $expected = <<<'PHP'
+[
+    (object) [
+        'callback' => function () {
+            return function (\Brick\VarExporter\Tests\Classes\PublicPropertiesOnly $a, int $b, string &$c, string ...$d) : ?string {
+                $a->foo += $b;
+                $c = $a->bar;
+                $a->bar = implode('', $d);
+                $this->someProp = [$a->foo, $a->bar, $c];
+                return $this->someProp['c'];
+            };
+        }
+    ]
+]
+PHP;
+
+        $this->assertExportEquals($expected, $var);
+    }
+}

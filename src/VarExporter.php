c582cf5a4890a2f111534a4bcf7d6f79476c90ea
Merlin <merlin@geeks4change.net>
2024-03-16 14:59:38 +0100

    Add CodeExporter, fixes #30

--- src/VarExporter.php
+++ src/VarExporter.php
@@ -55,6 +55,11 @@ final class VarExporter
     public const INLINE_SCALAR_LIST = 1 << 7;
 
     /**
+     * Any value implementing CodeInterface will print a literal expression.
+     */
+    public const ALLOW_CODE = 1 << 8;
+
+    /**
      * @deprecated Please use INLINE_SCALAR_LIST instead.
      */
     public const INLINE_NUMERIC_SCALAR_ARRAY = self::INLINE_SCALAR_LIST;
